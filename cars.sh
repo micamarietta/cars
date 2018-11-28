81db806599304535832f64b6730914168ddec5fc
Mica Marietta <marietta@chapman.edu>
2018-11-28 10:46:15 -0800

    Adds cars.sh

---
+++ cars.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#cars.sh
+#Mica Marietta
+
+
+echo "My Old Cars Program"
+
+N=1
+while[ $N -eq 1 ]
+echo "1 Add a car"
+echo "2 List your cars"
+echo "Q Quit"
+read answer
+
+#		if [ $answer -eq 1]
+#		then
+#			echo "What is the year of the car?"
+#			read year
+#			echo "What is the make of the car?"
+#			read make
+#			echo "What is the model of the car?"
+#			read model
+#			cat "$year:$make:$model" >> My_old_car
